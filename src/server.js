dfe237b857885bf329b16af5130034953cfef7d1
rkrafaelnascimento <rkrafael.nascimento@gmail.com>
2019-05-17 23:44:24 -0300

    adds Fastify web server

---
+++ src/server.js
@@ -0,0 +1,12 @@
+const fastify = require('fastify')({ logger: true })
+
+const start = async () => {
+  try {
+    await fastify.listen(process.env.PORT || 3000)
+    fastify.log.info(`server listening on ${fastify.server.address().port}`)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+start()
